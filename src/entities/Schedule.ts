b56b85125d1336cd6f798b7674a3dfd8cfe9a21f
Marc Canaleta <mcanaleta@gmail.com>
2025-02-08 08:00:09 +0100

    old typescript version cleanup

--- src/entities/Schedule.ts
+++
@@ -1,8 +0,0 @@
-export class GCBSchedule {
-  constructor(
-    public name: string,
-    public path: string,
-    public frequency: string,
-    public body: Record<string, any>
-  ) {}
-}
--- src/entities/Workflow.ts
+++
@@ -1,8 +0,0 @@
-export class GCBWorkflow {
-  constructor(
-    public name: string,
-    public path: string,
-    public body: string,
-    public description: string
-  ) {}
-}
